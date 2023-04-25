10b09b6755baddb8819df481f9374a96d9b36126
KiriGamaru <kirillobednin1@gmail.com>
2023-04-25 17:06:37 +0800

    Добавил разметку для дней/часов

--- app/src/main/java/com/example/wetherapp/MainViewModel.kt
+++ app/src/main/java/com/example/wetherapp/MainViewModel.kt
@@ -2,8 +2,9 @@ package com.example.wetherapp
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.wetherapp.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
-    val livaDataCurrent = MutableLiveData<String>()
-    val livaDataList = MutableLiveData<List<String>>()
+    val livaDataCurrent = MutableLiveData<WeatherModel>()
+    val livaDataList = MutableLiveData<List<WeatherModel>>()
 }
--- app/src/main/java/com/example/wetherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/wetherapp/fragments/MainFragment.kt
@@ -12,12 +12,24 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
 import com.example.wetherapp.R
+import com.example.wetherapp.adapters.VpAdapter
 import com.example.wetherapp.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+
 
 class MainFragment : Fragment() {
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "Hours",
+        "Days"
+    )
     private lateinit var pLauncher: ActivityResultLauncher<String>
-
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
@@ -28,10 +40,19 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding){
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp){
+                tab, pos -> tab.text = tList[pos]
+        }.attach()
+
     }
 
     private fun permissionListener(){
